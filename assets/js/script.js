4db711a9578fc2ad0c7a618b19068af3c5c571c5
tobbe8808 <Tobias_sorensen88@hotmail.se>
2022-03-18 15:19:22 +0000

    added all images and added img src for the 12 images

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+
+const section = document.querySelector("section");
+const Gamelives = document.querySelector("span");
+const Gamelivescount = 8;
+
+Gamelivescount.textContent = Gamelives;
+
+
+const getData = () => [
+
+    { imgSrc: "assets/images/dog1.jpg", name: "dog1" },
+    { imgSrc: "assets/images/dog2.jpg", name: "dog2" },
+    { imgSrc: "assets/images/dog3.jpg", name: "dog3" },
+    { imgSrc: "assets/images/dog4.jpg", name: "dog4" },
+    { imgSrc: "assets/images/dog5.jpg", name: "dog5" },
+    { imgSrc: "assets/images/dog6.jpg", name: "dog6" },
+    { imgSrc: "assets/images/dog7.jpg", name: "dog7" },
+    { imgSrc: "assets/images/dog8.jpg", name: "dog8" },
+    { imgSrc: "assets/images/dog9.jpg", name: "dog9" },
+    { imgSrc: "assets/images/dog10.jpg", name: "dog10" },
+    { imgSrc: "assets/images/dog11.jpg", name: "dog11" },
+    { imgSrc: "assets/images/dog12.jpg", name: "dog12" },
+    { imgSrc: "assets/images/dog1.jpg", name: "dog1" },
+    { imgSrc: "assets/images/dog2.jpg", name: "dog2" },
+    { imgSrc: "assets/images/dog3.jpg", name: "dog3" },
+    { imgSrc: "assets/images/dog4.jpg", name: "dog4" },
+    { imgSrc: "assets/images/dog5.jpg", name: "dog5" },
+    { imgSrc: "assets/images/dog6.jpg", name: "dog6" },
+    { imgSrc: "assets/images/dog7.jpg", name: "dog7" },
+    { imgSrc: "assets/images/dog8.jpg", name: "dog8" },
+    { imgSrc: "assets/images/dog9.jpg", name: "dog9" },
+    { imgSrc: "assets/images/dog10.jpg", name: "dog10" },
+    { imgSrc: "assets/images/dog11.jpg", name: "dog11" },
+    { imgSrc: "assets/images/dog12.jpg", name: "dog12" },
+    ];
+
+    
+  
